426dd9dda82d4aaacc96398333d72163d0e7cfa8
David Antuña Rodríguez <davidanrod@gmail.com>
2018-03-22 10:04:52 +0100

    Style and added reset function to entities (boards created only once)

---
+++ src/entities.js
@@ -0,0 +1,271 @@
+/////////////// BEGIN PLAYER RELATED ENTITIES ///////////////
+const Beer = function(x, y, vx) {
+  this.setup('BEER', { vx: -vx });
+  this.x = x;
+  this.y = y;
+
+  this.reset = function() {
+    this.board.remove(this);
+  };
+};
+
+Beer.prototype = new Sprite();
+Beer.prototype.type = OBJECT_PLAYER_PROJECTILE;
+
+Beer.prototype.step = function(dt) {
+  this.x += this.vx * dt;
+};
+
+/**
+ * This player moves or throws beer for every click.
+ * Long key press won't have any effect
+ */
+const Player = function() {
+  this.setup('PLAYER', {
+    fixedPos: [
+      { x: 325, y: 90 },
+      { x: 357, y: 185 },
+      { x: 389, y: 281 },
+      { x: 421, y: 377 },
+    ],
+    initialPos: 3,
+    currentPos: this.initialPos,
+    beer: new Beer(0, 0, 0),
+  });
+
+  this.reset = function() {
+    this.currentPos = this.initialPos;
+
+    this.x = this.fixedPos[this.currentPos].x;
+    this.y = this.fixedPos[this.currentPos].y;
+  };
+  this.reset();
+
+  let freeUp = false; // Control over key 'up' press
+  let freeDown = false; // Control over key 'down' press
+  let freeSpace = false; // Control over key 'space' press
+
+  this.step = function() {
+    if (!Game.keys['up']) freeUp = true;
+    if (!Game.keys['down']) freeDown = true;
+    if (!Game.keys['space']) freeSpace = true;
+
+    if (freeUp && Game.keys['up']) {
+      freeUp = false;
+      this.currentPos =
+        this.currentPos == 0 ? this.fixedPos.length - 1 : --this.currentPos;
+    } else if (freeDown && Game.keys['down']) {
+      freeDown = false;
+      this.currentPos = ++this.currentPos % this.fixedPos.length;
+    } else if (freeSpace && Game.keys['space']) {
+      freeSpace = false;
+      this.board.add(
+        Object.create(this.beer, {
+          x: {
+            value: this.x - Sprites.BEER.w,
+            enumerable: true,
+            writable: true,
+          },
+          y: {
+            value: this.y,
+            enumerable: true,
+            writable: true,
+          },
+          vx: {
+            value: -200,
+            enumerable: true,
+          },
+        })
+      );
+    }
+
+    this.x = this.fixedPos[this.currentPos].x;
+    this.y = this.fixedPos[this.currentPos].y;
+  };
+};
+
+Player.prototype = new Sprite();
+Player.prototype.type = OBJECT_PLAYER;
+
+/**
+ * This player moves or throws beer for every reload tick.
+ * Long key press will trigger multiple actions.
+ */
+var Player2 = function() {
+  this.setup('PLAYER', {
+    fixedPos: [
+      { x: 325, y: 90 },
+      { x: 357, y: 185 },
+      { x: 389, y: 281 },
+      { x: 421, y: 377 },
+    ],
+    initialPos: 3,
+    currentPos: initialPos,
+    beer: new Beer(0, 0, 0),
+    reloadTime: 0.25,
+    restTime: 0.5,
+  });
+
+  this.reset = function() {
+    this.currentPos = this.initialPos;
+
+    this.reload = this.reloadTime;
+    this.rest = this.restTime;
+    this.x = this.fixedPos[this.currentPos].x;
+    this.y = this.fixedPos[this.currentPos].y;
+  };
+  this.reset();
+
+  this.step = function(dt) {
+    this.rest -= dt;
+    if (Game.keys['up'] && this.rest < 0) {
+      Game.keys['up'] = false;
+      this.rest = this.restTime;
+      this.currentPos =
+        this.currentPos == 0 ? this.fixedPos.length - 1 : --this.currentPos;
+    } else if (Game.keys['down'] && this.rest < 0) {
+      Game.keys['down'] = false;
+      this.rest = this.restTime;
+      this.currentPos = ++this.currentPos % this.fixedPos.length;
+    }
+
+    this.x = this.fixedpPos[this.currentPos].x;
+    this.y = this.fixedPos[this.currentPos].y;
+
+    this.reload -= dt;
+    if (Game.keys['space'] && this.reload < 0) {
+      Game.keys['space'] = false;
+      this.reload = this.reloadTime;
+
+      this.board.add(
+        Object.create(this.beer, {
+          x: {
+            value: this.x - Sprites.BEER.w,
+            enumerable: true,
+            writable: true,
+          },
+          y: {
+            value: this.y,
+            enumerable: true,
+            writable: true,
+          },
+          vx: {
+            value: -200,
+            enumerable: true,
+          },
+        })
+      );
+    }
+  };
+};
+
+Player2.prototype = new Sprite();
+Player2.prototype.type = OBJECT_PLAYER;
+/////////////// END PLAYER RELATED ENTITIES ///////////////
+
+/////////////// BEGIN NPC RELATED ENTITIES ///////////////
+var Glass = function(x, y, vx) {
+  this.setup('GLASS');
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+
+  this.reset = function() {
+    this.board.remove(this);
+  };
+};
+
+Glass.prototype = new Sprite();
+Glass.prototype.type = OBJECT_CLIENT_PROJECTILE;
+
+Glass.prototype.step = function(dt) {
+  this.x += this.vx * dt;
+
+  const collision = this.board.collide(this, OBJECT_PLAYER);
+  if (collision) {
+    this.board.remove(this);
+    GameManager.notifyServed();
+  }
+};
+
+var Client = function(x, y, vx, sprite) {
+  this.setup(sprite, { reloadTime: 0.25 });
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+
+  this.reset = function() {
+    this.board.remove(this);
+  };
+};
+
+Client.prototype = new Sprite();
+Client.prototype.type = OBJECT_CLIENT;
+
+Client.prototype.step = function(dt) {
+  this.x += this.vx * dt;
+
+  const collision = this.board.collide(this, OBJECT_PLAYER_PROJECTILE);
+  if (collision) {
+    collision.hit(this);
+    this.board.add(new Glass(this.x, this.y + 10, 200));
+    this.board.remove(this);
+
+    GameManager.notifyGlass();
+  }
+};
+/////////////// END NPC RELATED ENTITIES ///////////////
+
+/////////////// BEGIN GAME RELATED ENTITIES ///////////////
+const Background = function() {
+  this.setup('BACKGROUND');
+  this.x = 0;
+  this.y = 0;
+
+  this.step = function() {};
+
+  this.reset = function() {};
+};
+
+Background.prototype = new Sprite();
+
+const Leftwall = function() {
+  this.setup('LEFT_WALL');
+  this.x = 0;
+  this.y = 0;
+
+  this.step = function() {};
+
+  this.reset = function() {};
+};
+
+Leftwall.prototype = new Sprite();
+
+var DeadZone = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.w = 10;
+  this.h = 80;
+  this.type = OBJECT_DEADZONE;
+
+  this.step = function() {
+    const collision = this.board.collide(
+      this,
+      OBJECT_CLIENT | OBJECT_PLAYER_PROJECTILE | OBJECT_CLIENT_PROJECTILE
+    );
+
+    if (collision) {
+      collision.hit(this);
+
+      GameManager.notifyDead();
+    }
+  };
+
+  this.draw = function() {
+    Game.ctx.fillStyle = '#5e7be3';
+    Game.ctx.fillRect(this.x, this.y, this.w, this.h);
+  };
+
+  this.reset = function() {};
+};
+/////////////// END GAME RELATED ENTITIES ///////////////
--- src/game.js
+++ src/game.js
@@ -1,11 +1,11 @@
 /** @enum */
 const Sprites = {
   BACKGROUND: { sx: 0, sy: 480, w: 512, h: 480, frames: 1 },
-  PLAYER: { sx: 512, sy: 0, w: 56, h: 66, frames: 1},
-  NPC: { sx: 512, sy: 66, w: 33, h: 33, frames: 1},
-  BEER: { sx: 512, sy: 99, w: 23, h: 32, frames: 1},
-  GLASS: { sx: 512, sy: 131, w: 23, h: 32, frames: 1},
-  LEFT_WALL: { sx: 0, sy: 0, w: 512, h: 480, frames: 1},
+  PLAYER: { sx: 512, sy: 0, w: 56, h: 66, frames: 1 },
+  NPC: { sx: 512, sy: 66, w: 33, h: 33, frames: 1 },
+  BEER: { sx: 512, sy: 99, w: 23, h: 32, frames: 1 },
+  GLASS: { sx: 512, sy: 131, w: 23, h: 32, frames: 1 },
+  LEFT_WALL: { sx: 0, sy: 0, w: 512, h: 480, frames: 1 },
 };
 
 const OBJECT_PLAYER = 1;
@@ -15,11 +15,11 @@ const OBJECT_CLIENT_PROJECTILE = 8;
 const OBJECT_DEADZONE = 16;
 
 const CLIENTS_FIXED_POS = [
-  {x: 110, y: 80},
-  {x: 78, y: 175},
-  {x: 46, y: 271},
-  {x: 14, y: 367},
-]
+  { x: 110, y: 80 },
+  { x: 78, y: 175 },
+  { x: 46, y: 271 },
+  { x: 14, y: 367 },
+];
 
 /** @enum */
 const BoardIds = {
@@ -29,7 +29,7 @@ const BoardIds = {
   BACKGROUND: 3,
   ENTITIES: 4,
   LEFT_WALL: 5,
-}
+};
 
 const startGame = function() {
   const ua = navigator.userAgent.toLowerCase();
@@ -42,21 +42,21 @@ const setup = function() {
   //////////////// Title screen ////////////////
   const titleBoard = new GameBoard();
   titleBoard.add(
-    new TitleScreen("Tapper", "Press space to start playing", playGame)
+    new TitleScreen('Tapper', 'Press space to start playing', playGame)
   );
   Game.setBoard(BoardIds.TITLE, titleBoard);
 
   //////////////// Win screen ////////////////
   const winBoard = new GameBoard();
   winBoard.add(
-    new TitleScreen("You win!", "Press space to start playing", playGame)
+    new TitleScreen('You win!', 'Press space to start playing', playGame)
   );
   Game.setBoard(BoardIds.WIN, winBoard);
 
   //////////////// Lose screen ////////////////
   const loseBoard = new GameBoard();
   loseBoard.add(
-    new TitleScreen("You lose!", "Press space to start playing", playGame)
+    new TitleScreen('You lose!', 'Press space to start playing', playGame)
   );
   Game.setBoard(BoardIds.LOSE, loseBoard);
 
@@ -91,11 +91,6 @@ const setup = function() {
   //////////////// Left wall ////////////////
   const frontboard = new GameBoard();
   frontboard.add(new Leftwall());
-  // left side deadzones so they are visible
-  frontboard.add(new DeadZone(100, 80));
-  frontboard.add(new DeadZone(68, 175));
-  frontboard.add(new DeadZone(36, 271));
-  frontboard.add(new DeadZone(4, 367));
   Game.setBoard(BoardIds.LEFT_WALL, frontboard);
 };
 
@@ -132,18 +127,24 @@ const loseGame = function() {
   Game.activateBoard(BoardIds.LOSE);
 };
 
-window.addEventListener("load", function() {
-  Game.initialize("game", Sprites, startGame);
+window.addEventListener('load', function() {
+  Game.initialize('game', Sprites, startGame);
 });
 
 const Spawner = function(coord, nclients, type, f) {
-  this.nclients = nclients;
+  this.initClients = nclients;
+  this.nclients = this.initClients;
   this.type = type;
   this.f = f;
   this.time = this.f;
   this.client = new Client(coord.x, coord.y, 100, type);
 
-  GameManager.notifyClients(this.nclients);
+  this.reset = function() {
+    this.time = this.f;
+    this.nclients = this.initClients;
+    GameManager.notifyClients(this.nclients);
+  };
+  this.reset();
 
   this.step = function(dt) {
     this.time -= dt;
@@ -159,250 +160,8 @@ const Spawner = function(coord, nclients, type, f) {
   this.draw = function() {};
 };
 
-/////////////// BEGIN PLAYER RELATED ENTITIES ///////////////
-const Beer = function(x, y, vx) {
-  this.setup('BEER', {vx: -vx});
-  this.x = x;
-  this.y = y;
-};
-
-Beer.prototype = new Sprite();
-Beer.prototype.type = OBJECT_PLAYER_PROJECTILE;
-
-Beer.prototype.step = function(dt) {
-  this.x += this.vx * dt;
-};
-
-/**
- * This player moves or throws beer for every click.
- * Long key press won't have any effect
- */
-const Player = function() {
-  this.setup('PLAYER', {
-    fixedPos: [
-      {x: 325, y: 90},
-      {x: 357, y: 185},
-      {x: 389, y: 281},
-      {x: 421, y: 377},
-    ],
-    currentPos: 3,
-    beer: new Beer(0, 0, 0),
-  });
-  this.x = this.fixedPos[this.currentPos].x;
-  this.y = this.fixedPos[this.currentPos].y;
-
-  let freeUp = false; // Control over key 'up' press
-  let freeDown = false; // Control over key 'down' press
-  let freeSpace = false; // Control over key 'space' press
-
-  this.step = function() {
-    if (!Game.keys['up']) freeUp = true;
-    if (!Game.keys['down']) freeDown = true;
-    if (!Game.keys['space']) freeSpace = true;
-
-    if (freeUp && Game.keys['up']) {
-      freeUp = false;
-      this.currentPos = this.currentPos == 0 ?
-          this.fixedPos.length - 1 :
-          --this.currentPos;
-    }
-    else if (freeDown && Game.keys['down']) {
-      freeDown = false;
-      this.currentPos = ++this.currentPos % this.fixedPos.length;
-    }
-    else if (freeSpace && Game.keys['space']) {
-      freeSpace = false;
-      this.board.add(Object.create(this.beer, {
-        'x': {
-          value: this.x - Sprites.BEER.w,
-          enumerable: true,
-          writable: true
-        },
-        'y': {
-          value: this.y,
-          enumerable: true,
-          writable: true
-        },
-        'vx': {
-          value: -200,
-          enumerable: true
-        },
-      }));
-    }
-
-    this.x = this.fixedPos[this.currentPos].x;
-    this.y = this.fixedPos[this.currentPos].y;
-  };
-};
-
-Player.prototype = new Sprite();
-Player.prototype.type = OBJECT_PLAYER;
-
-/**
- * This player moves or throws beer for every reload tick.
- * Long key press will trigger multiple actions.
- */
-var Player2 = function() {
-  this.setup('PLAYER', {
-    fixedPos: [
-      {x: 325, y: 90},
-      {x: 357, y: 185},
-      {x: 389, y: 281},
-      {x: 421, y: 377},
-    ],
-    currentPos: 3,
-    beer: new Beer(0, 0, 0),
-    reloadTime: 0.25,
-    restTime: 0.5,
-  });
-
-  this.reload = this.reloadTime;
-  this.rest = this.restTime;
-  this.x = this.fixedPos[this.currentPos].x;
-  this.y = this.fixedPos[this.currentPos].y;
-
-  this.step = function(dt) {
-    this.rest -= dt;
-    if (Game.keys['up'] && this.rest < 0) {
-      Game.keys['up'] = false;
-      this.rest = this.restTime;
-      this.currentPos = this.currentPos == 0 ?
-          this.fixedPos.length - 1 :
-          --this.currentPos;
-    }
-    else if (Game.keys['down'] && this.rest < 0) {
-      Game.keys['down'] = false;
-      this.rest = this.restTime;
-      this.currentPos = ++this.currentPos % this.fixedPos.length;
-    }
-
-    this.x = this.fixedpPos[this.currentPos].x;
-    this.y = this.fixedPos[this.currentPos].y;
-
-    this.reload -= dt;
-    if (Game.keys['space'] && this.reload < 0) {
-      Game.keys['space'] = false;
-      this.reload = this.reloadTime;
-
-      this.board.add(Object.create(this.beer, {
-        'x': {
-          value: this.x - Sprites.BEER.w,
-          enumerable: true,
-          writable: true
-        },
-        'y': {
-          value: this.y,
-          enumerable: true,
-          writable: true
-        },
-        'vx': {
-          value: -200,
-          enumerable: true
-        },
-      }));
-    }
-  };
-};
-
-Player2.prototype = new Sprite();
-Player2.prototype.type = OBJECT_PLAYER;
-/////////////// END PLAYER RELATED ENTITIES ///////////////
-
-
-/////////////// BEGIN NPC RELATED ENTITIES ///////////////
-var Glass = function(x, y, vx) {
-  this.setup('GLASS');
-  this.x = x;
-  this.y = y;
-  this.vx = vx;
-};
-
-Glass.prototype = new Sprite();
-Glass.prototype.type = OBJECT_CLIENT_PROJECTILE;
-
-Glass.prototype.step = function(dt)  {
-  this.x += this.vx * dt;
-
-  const collision = this.board.collide(this, OBJECT_PLAYER);
-  if (collision) {
-    this.board.remove(this);
-    GameManager.notifyServed();
-  }
-};
-
-var Client = function(x, y, vx, sprite) {
-  this.setup(sprite, {reloadTime: 0.25});
-  this.x = x;
-  this.y = y;
-  this.vx = vx;
-};
-
-Client.prototype = new Sprite();
-Client.prototype.type = OBJECT_CLIENT;
-
-Client.prototype.step = function(dt) {
-  this.x += this.vx * dt;
-
-  const collision = this.board.collide(this, OBJECT_PLAYER_PROJECTILE);
-  if (collision) {
-    collision.hit(this);
-    this.board.add(new Glass(this.x, this.y + 10, 200));
-    this.board.remove(this);
-
-    GameManager.notifyGlass();
-  }
-};
-/////////////// END NPC RELATED ENTITIES ///////////////
-
-
-/////////////// BEGIN GAME RELATED ENTITIES ///////////////
-const Background = function() {
-  this.setup('BACKGROUND');
-  this.x = 0;
-  this.y = 0;
-
-  this.step = function() {};
-};
-
-Background.prototype = new Sprite();
-
-const Leftwall = function() {
-  this.setup('LEFT_WALL');
-  this.x = 0;
-  this.y = 0;
-
-  this.step = function() {};
-};
-
-Leftwall.prototype = new Sprite();
-
-var DeadZone = function(x, y) {
-  this.x = x;
-  this.y = y;
-  this.w = 10;
-  this.h = 80;
-  this.type = OBJECT_DEADZONE;
-
-  this.step = function() {
-    const collision = this.board.collide(this,
-        OBJECT_CLIENT | OBJECT_PLAYER_PROJECTILE | OBJECT_CLIENT_PROJECTILE);
-
-    if (collision) {
-      collision.hit(this);
-
-      GameManager.notifyDead();
-    }
-  };
-
-  this.draw = function() {
-    Game.ctx.fillStyle = '#5e7be3';
-    Game.ctx.fillRect(this.x, this.y, this.w, this.h);
-  };
-}
-/////////////// END GAME RELATED ENTITIES ///////////////
-
 /////////////// BEGIN GAME MANAGER ///////////////
-const GameManager = new function () {
+const GameManager = new function() {
   this.npcs = 0;
   this.glasscount = 0;
   this.dead = 0;
@@ -425,6 +184,7 @@ const GameManager = new function () {
   this.notifyServed = function() {
     --this.glasscount;
     --this.npcs;
+
     this.check();
   };
 
@@ -434,12 +194,11 @@ const GameManager = new function () {
   };
 
   this.check = function() {
-    if (!this.npcs && !this.glasscount) {
+    if (this.dead) {
+      loseGame();
+    } else if (!this.npcs && !this.glasscount) {
       winGame();
     }
-    else if (this.dead) {
-      loseGame();
-    }
   };
-};
+}();
 /////////////// END GAME MANAGER ///////////////
